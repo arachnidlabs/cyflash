9754b7382f1a2393216fc3f0fc543c646aa3204b
Dan Yerushalmi <dan@irpsystems.com>
2019-08-13 10:20:13 +0300

    updating batch file to creating exe

--- build_app_win/run_build.bat
+++ build_app_win/run_build.bat
@@ -1,4 +1,4 @@
-pyinstaller ../cyflash/bootload.py --path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\Remote Debugger\x86" ^
+pyinstaller ../cyflash_run.py --path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\Remote Debugger\x86" ^
 -n cyflash --path=../cyflash/ --onefile ^
 --hidden-import=can.interfaces.pcan ^
 --hidden-import=can.interfaces.ixxat ^
